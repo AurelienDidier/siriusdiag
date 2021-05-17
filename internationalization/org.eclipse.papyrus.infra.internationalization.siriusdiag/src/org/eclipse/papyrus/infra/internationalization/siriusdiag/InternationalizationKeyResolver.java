c490bb913584bc50a676c5a8c2b35a8803ce902e
Aurelien Didier <aurelien.didier51@gmail.com>
2021-05-17 09:41:55 +0200

    Add all internationalization modification.
    Signed-off-by: Aurelien Didier <aurelien.didier51@gmail.com>

---
+++ internationalization/org.eclipse.papyrus.infra.internationalization.siriusdiag/src/org/eclipse/papyrus/infra/internationalization/siriusdiag/InternationalizationKeyResolver.java
@@ -0,0 +1,159 @@
+/*****************************************************************************
+ * Copyright (c) 2016 CEA LIST and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Nicolas FAUVERGUE (CEA LIST) nicolas.fauvergue@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+
+package org.eclipse.papyrus.infra.internationalization.siriusdiag;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.gmf.runtime.notation.Diagram;
+//import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.papyrus.infra.internationalization.InternationalizationEntry;
+import org.eclipse.papyrus.infra.internationalization.InternationalizationFactory;
+import org.eclipse.papyrus.infra.internationalization.siriusdiag.utils.QualifiedNameUtils;
+import org.eclipse.sirius.diagram.DSemanticDiagram;
+
+/**
+ * The internationalization key resolver for the infra elements (Diagrams an Tables).
+ */
+public class InternationalizationKeyResolver extends org.eclipse.papyrus.infra.internationalization.utils.InternationalizationKeyResolver {
+
+	/**
+	 * The '_labelDiagram_' prefix of each diagram entry.
+	 */
+	protected static final String LABEL_SIRIUS_DIAGRAM_PREFIX_QN = "_labelSiriusDiagram_"; //$NON-NLS-1$
+
+	// /**
+	// * The '_labelTable_' prefix of each table entry.
+	// */
+	// protected static final String LABEL_TABLE_PREFIX_QN = "_labelTable_"; //$NON-NLS-1$
+
+	/**
+	 * The '_label_' prefix of each entry.
+	 */
+	public static final String LABEL_PREFIX = "_label_"; //$NON-NLS-1$
+
+	/**
+	 * The notation extension file (cannot use the
+	 * NotationModel.NOTATION_FILE_EXTENSION because of dependencies cycle).
+	 */
+	protected static final String AIRD_FILE_EXTENSION = "aird"; //$NON-NLS-1$
+
+	/**
+	 * Constructor.
+	 */
+	public InternationalizationKeyResolver() {
+		// Do nothing
+	}
+
+	/**
+	 * This allows to create an internationalization entry corresponding to the
+	 * key in parameter and creating table or diagram reference if possible.
+	 *
+	 * @param key
+	 *            The key.
+	 * @param resourceSet
+	 *            The current resource set.
+	 * @param uri
+	 *            The uri of the file to manage.
+	 * @return The created internationalization entry.
+	 */
+	@Override
+	public InternationalizationEntry createInternationalizationEntryByKey(final String key, final ResourceSet resourceSet,
+			final URI uri) {
+		final InternationalizationEntry entry = InternationalizationFactory.eINSTANCE.createInternationalizationEntry();
+		if (key.startsWith(LABEL_SIRIUS_DIAGRAM_PREFIX_QN)) {
+			final String keyWithoutPrefix = key.substring(LABEL_SIRIUS_DIAGRAM_PREFIX_QN.length());
+			final String qualifiedName = keyWithoutPrefix.substring(0, keyWithoutPrefix.indexOf(LABEL_PREFIX));
+			final String diagramName = keyWithoutPrefix
+					.substring(keyWithoutPrefix.indexOf(LABEL_PREFIX) + LABEL_PREFIX.length());
+
+			final Resource notationResource = resourceSet
+					.getResource(uri.trimFileExtension().appendFileExtension(AIRD_FILE_EXTENSION), true);
+
+			if (null != notationResource && null != notationResource.getContents()
+					&& !notationResource.getContents().isEmpty()) {
+				final DSemanticDiagram foundDiagram = QualifiedNameUtils.getSiriusDiagram(notationResource, diagramName,
+						qualifiedName);
+				entry.setKey(foundDiagram);
+			}
+
+			// else if (key.startsWith(LABEL_TABLE_PREFIX_QN)) {
+			// final String keyWithoutPrefix = key.substring(LABEL_TABLE_PREFIX_QN.length());
+			// final String qualifiedName = keyWithoutPrefix.substring(0, keyWithoutPrefix.indexOf(LABEL_PREFIX));
+			// final String tableName = keyWithoutPrefix
+			// .substring(keyWithoutPrefix.indexOf(LABEL_PREFIX) + LABEL_PREFIX.length());
+			//
+			// final Resource umlResource = resourceSet
+			// .getResource(uri.trimFileExtension().appendFileExtension(NOTATION_FILE_EXTENSION), true);
+			//
+			// if (null != umlResource && null != umlResource.getContents() && !umlResource.getContents().isEmpty()) {
+			// final Table foundTable = QualifiedNameUtils.getTable(umlResource, tableName, qualifiedName);
+			// entry.setKey(foundTable);
+			// }
+		} else
+
+		{
+			entry.setKey(key);
+		}
+
+		return entry;
+	}
+
+	/**
+	 * This allows to get the entry key (can be override to get element string
+	 * identifier instead of string key).
+	 *
+	 * @param entry
+	 *            The internationalization entry.
+	 * @return The key as String.
+	 */
+	@Override
+	public String getKey(final InternationalizationEntry entry) {
+		final StringBuilder result = new StringBuilder();
+		if (entry.getKey() instanceof Diagram) {
+			result.append(LABEL_SIRIUS_DIAGRAM_PREFIX_QN);
+			final DSemanticDiagram diagram = (DSemanticDiagram) entry.getKey();
+			final EObject diagramContainer = diagram.getTarget();
+			result.append(QualifiedNameUtils.getQualifiedName(diagramContainer));
+			result.append(LABEL_PREFIX);
+			result.append(diagram.getName());
+			// }
+			// else if (entry.getKey() instanceof Table) {
+			// result.append(LABEL_TABLE_PREFIX_QN);
+			// final Table table = (Table) entry.getKey();
+			// final EObject tableContainer = table.getOwner();
+			// result.append(QualifiedNameUtils.getQualifiedName(tableContainer));
+			// result.append(LABEL_PREFIX);
+			// result.append(table.getName());
+		} else
+
+		{
+			result.append((String) entry.getKey());
+		}
+		return result.toString();
+
+	}
+
+	/**
+	 * This allows to dispose the key resolver.
+	 */
+	@Override
+	public void dispose() {
+		// Do nothing here
+	}
+
+}
---
+++ internationalization/org.eclipse.papyrus.infra.internationalization.siriusdiag/src/org/eclipse/papyrus/infra/internationalization/siriusdiag/utils/LabelInternationalizationPreferencesUtils.java
@@ -0,0 +1,87 @@
+/*****************************************************************************
+ * Copyright (c) 2016 CEA LIST and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Nicolas FAUVERGUE (ALL4TEC) nicolas.fauvergue@all4tec.net - Initial API and implementation
+ *
+ *****************************************************************************/
+
+package org.eclipse.papyrus.infra.internationalization.siriusdiag.utils;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.papyrus.infra.internationalization.common.utils.InternationalizationPreferencesUtils;
+import org.eclipse.sirius.diagram.DSemanticDiagram;
+
+/**
+ * Open the label internationalization preferences to the others plugins with the utils plugin.
+ */
+public class LabelInternationalizationPreferencesUtils {
+
+	/**
+	 * This allows to modify the internationalization preference value.
+	 *
+	 * @param eObject
+	 *            The {@link EObject) corresponding (to get its resource).
+	 * @param value
+	 *            The new preference value.
+	 */
+	public static void setInternationalizationPreference(final EObject eObject, final boolean value) {
+		if (eObject instanceof DSemanticDiagram) {
+			LabelInternationalization.getInstance().setInternationalizationPreference((DSemanticDiagram) eObject, value);
+			// } else if (eObject instanceof Diagram) {
+			// LabelInternationalization.getInstance().setInternationalizationPreference((Diagram) eObject, value);
+		} else {
+			InternationalizationPreferencesUtils.setInternationalizationPreference(eObject, value);
+		}
+	}
+
+	/**
+	 * This allows to modify the internationalization preference value.
+	 *
+	 * @param resource
+	 *            The {@link Resource) to get the papyrus project preferences.
+	 * @param value
+	 *            The new preference value.
+	 */
+	public static void setInternationalizationPreference(final Resource resource, final boolean value) {
+		InternationalizationPreferencesUtils.setInternationalizationPreference(resource, value);
+	}
+
+	/**
+	 * This allows to get the internationalization preference value.
+	 *
+	 * @param eObject
+	 *            The {@link EObject) corresponding (to get its resource).
+	 * @return <code>true</code> if the preference value is set to true, <code>false</code> otherwise.
+	 */
+	public static boolean getInternationalizationPreference(final EObject eObject) {
+		boolean result = false;
+		if (eObject instanceof DSemanticDiagram) {
+			result = LabelInternationalization.getInstance().getInternationalizationPreference((DSemanticDiagram) eObject);
+			// } else if (eObject instanceof Diagram) {
+			// result = LabelInternationalization.getInstance().getInternationalizationPreference((Diagram) eObject);
+		} else {
+			result = InternationalizationPreferencesUtils.getInternationalizationPreference(eObject);
+		}
+		return result;
+	}
+
+	/**
+	 * This allows to get the internationalization preference value.
+	 *
+	 * @param resource
+	 *            The {@link Resource) to get the papyrus project preferences.
+	 * @return <code>true</code> if the preference value is set to true, <code>false</code> otherwise.
+	 */
+	public static boolean getInternationalizationPreference(final Resource resource) {
+		return InternationalizationPreferencesUtils.getInternationalizationPreference(resource);
+	}
+}
